9289d9b9bedffd816dae96d9e4f11404aa7fb7e1
itsyouyouAgain <qing.yo@northeastern.edu>
2024-04-27 03:55:55 -0700

    initialize project

---
+++ moblie-app-ws/src/main/java/com/springboot/app/ws/ui/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.springboot.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users") // http://localhost:8080/users
+// this controller will be responsible for all operations that have to do with users, url will reflect
+public class UserController {
+
+}
